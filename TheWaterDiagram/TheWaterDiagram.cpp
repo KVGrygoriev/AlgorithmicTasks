8786a084aa464c713e6e2d3c3360c36c372e6dc6
KVGrygoriev <KVGrygoriev@luxoft.com>
2019-03-05 11:19:32 +0200

    Change method name

--- TheWaterDiagram/TheWaterDiagram.cpp
+++ TheWaterDiagram/TheWaterDiagram.cpp
@@ -2,13 +2,14 @@
 #include <sstream>
 
 #include <vector>
+#include <map>
 
 using namespace std;
 
 const int NO_PARAMS_PASS = 1;
 
 template <class Container>
-void splitStringFillContainer(const string & _srcStr, Container & _dstContainer, char _delimeter = ',')
+void convertStringToContainer(const string & _srcStr, Container & _dstContainer, char _delimeter = ',')
 {
     stringstream ss(_srcStr);
     string token;
@@ -16,13 +17,15 @@ void splitStringFillContainer(const string & _srcStr, Container & _dstContainer,
     while (getline(ss, token, _delimeter)) {
 		try
 		{
-			_dstContainer.push_back(stoi(token));
+			auto convertedValue = stoi(token);
 			
-			if (_dstContainer.back() < 0)
+			if (convertedValue < 0)
 			{
 				errno = ERANGE;
 				throw std::out_of_range("Value should be positive, according to task description");
 			}
+			
+			_dstContainer.push_back(convertedValue);
 		}
 		catch (const std::exception& e)
 		{
@@ -34,23 +37,23 @@ void splitStringFillContainer(const string & _srcStr, Container & _dstContainer,
 		
 int main(int argc, char* argv[])
 {
-	vector<int> fieldConfiguration; 
+	vector<int> fieldConfiguration;
 		
 	if (NO_PARAMS_PASS == argc)
 	{
 		string defaultField = "5,2,1,1,0,1,1,2,2,4";	
 		cout << "Uses default field configuration " << defaultField << endl;
-		splitStringFillContainer(defaultField, fieldConfiguration);
+		convertStringToContainer(defaultField, fieldConfiguration);
 	}
 	else
 	{
 		cout << "Uses user field configuration " << argv << endl;
-		splitStringFillContainer(string(argv[1]), fieldConfiguration);
+		convertStringToContainer(string(argv[1]), fieldConfiguration);
 	}
 
 	
-	//for(auto x : fieldConfiguration)
-	//	cout << x;
+	//for(auto x : sortedFieldConfiguration)
+	//	cout << x.first << " " << x.second << endl;
 	
 	return 0;
 }
